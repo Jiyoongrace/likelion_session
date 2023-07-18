b68527189f6a99d5295ee4796b8c195a4c2fca83
jiyoongrace <giyun3632@naver.com>
2023-07-18 13:04:17 +0900

    업로드

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Hello from './Hello';
+import Counter from './Counter';
+import MovieList from './MovieList';
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Hello name="저는 지윤입니다." color="blue"/>
+      <Counter />
+      <MovieList  />
+    </>
   );
 }
 
